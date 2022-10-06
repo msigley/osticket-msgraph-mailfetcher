b8320cf3894dabd1055445781e2076d4a6f946d9
Matthew Sigley <matthew.d.sigley@gmail.com>
2022-10-06 10:07:51 -0400

    Bumped version to 1.0.1

--- msgraph/plugin.php
+++ msgraph/plugin.php
@@ -2,7 +2,7 @@
 
 return array(
     'id' =>             'msigley:msgraph', # notrans
-    'version' =>        '1.0.0',
+    'version' =>        '1.0.1',
     'name' =>           /* trans */ 'Microsoft Graph API Mailfetcher',
     'author' =>         'Matthew Sigley',
     'description' =>    /* trans */ 'Fetches mail from Microsoft 365 using the Microsoft Graph API',
